999a75cb3a5970b98ee8ba81a10efb77fda7ef8a
Ruminot21 <gdruminot3@gmail.com>
2024-06-24 08:34:05 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 
 // MARK: Config
 const URL_JSON_CANALES_PRINCIPAL = 'https://raw.githubusercontent.com/Alplox/json-teles/main/canales.json';
-const URL_M3U_CANALES_IPTV = 'https://iptv-org.github.io/iptv/index.m3u';
+const URL_M3U_CANALES_IPTV = 'https://tv.teleclub.xyz/lista/canales.m3u';
 const TWITCH_PARENT = 'alplox.github.io';
 
 const CONTAINER_OVERLAY = document.querySelector('.container-overlay');
